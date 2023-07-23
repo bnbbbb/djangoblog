686b1e5d4b59f4a089cbea773d297f6a67962c7c
bongsu <zio7542@naver.com>
2023-07-23 23:05:27 +0900

    blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post, Tag, Comment, Category
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields ={'slug':('name',)}
+
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields ={'slug':('name',)}
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Category)
+admin.site.register(Tag)
